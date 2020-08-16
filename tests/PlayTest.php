2c0c3849b869bbcdbb467a1c8803ca9ba80130ff
@MIRROR_ <mirror785@gmail.com>
2020-08-16 14:20:20 +0900

    Add MML text test.

--- tests/PlayTest.php
+++ tests/PlayTest.php
@@ -37,4 +37,36 @@ class PlayTest extends TestCase
         fclose($handle);
         $this->assertTrue(file_exists($file));
     }
+
+    public function test_playMmlTextTest() {
+        $container = MmlContainer::parse(MmlSample::$penguin_text);
+
+        $player = new ApuMmlPlayer();
+        $player->setup(
+        ['AudioUnits' => [[
+            'Name' => 'unit1',
+            'Devices' => [
+                // device number (1:pulse1, 2:pulse2, 3:triangle, 4:noise)
+                // => ['Position' => [ -1.5 <= panning <= 1.5, scale offset ]]
+                1 => ['Position' => [-0.25 , 1.0]],
+                2 => ['Position' => [ 0.25 , 1.0]],
+                3 => ['Position' => [-0.125, 1.0]],
+                4 => ['Position' => [ 0.125, 1.0]],
+                ],
+            ]]]);
+
+        $player->sampleTime = 60.0;
+        $player->setDeviceParameter([
+            'unit1' => [
+                1 => ['Voice'=>2],
+                2 => ['Voice'=>2],
+            ]]);
+        $data = $player->play($container);
+
+        $file = __DIR__ . '/wav/MmlTextTest.wav';
+        $handle = fopen($file, 'wb');
+        fwrite($handle, makeWaveData($data, $player->channelCount, $player->sampleBits, $player->sampleRate));
+        fclose($handle);
+        $this->assertTrue(file_exists($file));
+    }
 }
